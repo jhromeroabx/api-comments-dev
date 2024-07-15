fc8df5d15aeed626a7ca4e3c6fc63d31de17a5b7
AntoCant <antonio.canteros.alberto@gmail.com>
2024-07-15 00:34:03 -0300

    first commit

---
+++ src/main/java/com/demo/api_comments/ApiCommentsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.api_comments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCommentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCommentsApplication.class, args);
+	}
+
+}
